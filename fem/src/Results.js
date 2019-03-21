b618c72a19bf413fef7e11dae83de452ad2a765f
汤娴 <tang@tang.local>
2019-03-21 23:13:56 +0800

    6thcommit

--- fem/src/Results.js
+++ fem/src/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pf from "petfinder-client";
 import Pet from "./Pet";
+import SearchBox from "./SearchBox";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -40,6 +41,7 @@ class Results extends React.Component {
   render() {
     return (
       <div className="search">
+        <SearchBox />
         {this.state.pets.map(pet => {
           let breed;
           if (Array.isArray(pet.breeds.breed)) {
